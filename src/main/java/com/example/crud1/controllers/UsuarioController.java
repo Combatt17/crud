a8ce954e88ffe39494d14405bc92a3b9950b2327
Combatt17 <carloscombatt17@gmail.com>
2021-10-28 07:10:30 -0500

    CRUD JAVA con Spring boot

---
+++ src/main/java/com/example/crud1/controllers/UsuarioController.java
@@ -0,0 +1,33 @@
+package com.example.crud1.controllers;
+
+import java.util.ArrayList;
+
+import com.example.crud1.models.UsuarioModel;
+import com.example.crud1.services.UsuarioService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/usuario")
+public class UsuarioController {
+    
+    @Autowired
+    UsuarioService usuarioService;
+
+    @GetMapping
+    public ArrayList<UsuarioModel> obtenerUsuarios(){
+        return usuarioService.getUsuarios();
+    }
+
+    @PostMapping
+    public UsuarioModel guardarUsuario(@RequestBody UsuarioModel usuario){
+        return usuarioService.guardarUsuario(usuario);
+    }
+
+
+}
---
+++ src/main/java/com/example/crud1/models/UsuarioModel.java
@@ -0,0 +1,53 @@
+package com.example.crud1.models;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "usuario")
+public class UsuarioModel {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(unique = true, nullable = false)
+    private Long id;
+
+
+
+    private String nombre;
+    private String correo;
+    private Integer puntaje;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public Integer getPuntaje() {
+        return puntaje;
+    }
+
+    public void setPuntaje(Integer puntaje) {
+        this.puntaje = puntaje;
+    }
+    
+
+}
